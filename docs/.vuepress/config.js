13a9f2d3fef234acd33aef23eb8990e63dea17a7
taokepppooo <1075626521@qq.com>
2024-09-12 16:34:21 +0800

    chore: 修改配置

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress/cli'
+import { viteBundler } from '@vuepress/bundler-vite'
+
+export default defineUserConfig({
+  lang: 'en-US',
+
+  base: '/',
+  title: 'VuePress',
+  description: 'My first VuePress Site',
+
+  theme: defaultTheme({
+    logo: 'https://vuejs.press/images/hero.png',
+
+    navbar: ['/', '/get-started'],
+  }),
+
+  bundler: viteBundler(),
+})
